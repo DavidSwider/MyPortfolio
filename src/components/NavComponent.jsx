1e3b2942ded2b66bbe228b773ceb6973dca38ba2
DavidSwider <davidswider6@gmail.com>
2025-04-22 19:51:19 -0400

    started adding nav and footer css

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -7,10 +7,10 @@ const NavComponent = () => {
         <nav className="navbar">
             <div className="navbar-brand">David Swider</div>
             <ul className="navbar-links">
-                <li><Link to="/aboutme">About Me</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/resume">Resume</Link></li>
+                <li><Link to="/aboutme"className="navbar-link">About Me</Link></li>
+                <li><Link to="/projects"className="navbar-link">Projects</Link></li>
+                <li><Link to="/contact"className="navbar-link">Contact</Link></li>
+                <li><Link to="/resume"className="navbar-link">Resume</Link></li>
             </ul>
         </nav>
     );
--- src/pages/AboutmePage.jsx
+++ src/pages/AboutmePage.jsx
@@ -4,7 +4,7 @@ import AboutmeComponent from '../components/AboutmeComponent';
 const AboutmePage = () => {
     return (
         <div>
-            <h1>About Me</h1>
+            <h1>A little to know about me</h1>
             <AboutmeComponent />
         </div>
     );
